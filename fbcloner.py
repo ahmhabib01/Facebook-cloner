b192734b505ab97924eb7cd916cb585d9c722962
Ahsan Habib <157474063+ahmhabib01@users.noreply.github.com>
2024-03-16 12:19:40 +0600

    Update fbcloner.py

--- fbcloner.py
+++ fbcloner.py
@@ -70,7 +70,7 @@ def clear():
         print(logo)
 
 ##------------(LOGO)------------##
-os.system('xdg-open https://www.facebook.com/profile.php?id=100054925285605')
+os.system('xdg-open https://www.facebook.com/profile.php?id=61554490682335')
 
 logo=(f"""{A}
 
